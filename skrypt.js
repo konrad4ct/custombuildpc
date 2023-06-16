ce072b1b098e0c1ba50e882ba1ed5aecdc56d2b1
konrad4ct <129171398+konrad4ct@users.noreply.github.com>
2023-06-16 08:40:05 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -1,4 +1,15 @@
-$(window).scroll(function(){
-    $('nav a').animate(function(){
-    });
-});
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("nav");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
